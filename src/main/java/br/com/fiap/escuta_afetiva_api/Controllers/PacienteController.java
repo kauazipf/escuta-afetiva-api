e8f2fde6be4b27b9f2d779a126667b3eeccd19d7
kauazipf <rm558957@fiap.com.br>
2025-03-07 10:23:53 -0300

    Ajustes no controller

--- src/main/java/br/com/fiap/escuta_afetiva_api/Controllers/PacienteController.java
+++ src/main/java/br/com/fiap/escuta_afetiva_api/Controllers/PacienteController.java
@@ -3,44 +3,80 @@ package br.com.fiap.escuta_afetiva_api.Controllers;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import br.com.fiap.escuta_afetiva_api.Models.Paciente;
 
 @RestController
 public class PacienteController {
 
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
     private List<Paciente> repository = new ArrayList<>();
 
-    @GetMapping("/pacientes")
-    public List<Paciente> index(){
+    // GETALL
+    @GetMapping(path = "/pacientes")
+    public List<Paciente> index() {
+        log.info("Buscando todas categorias");
         return repository;
     }
 
-    @PostMapping("/pacientes")
+    // POST
+    @PostMapping(path = "/pacientes")
     @ResponseStatus(code = HttpStatus.CREATED)
-    public ResponseEntity<String> criarPaciente(@RequestBody Paciente paciente) {
-        System.out.println("Cadastrando um novo paciente " + paciente.getNome() + paciente.getEmail() + paciente.getPlano() + paciente.getTelefone());
+    public ResponseEntity<Paciente> create(@RequestBody Paciente paciente) {
+        log.info("Cadastrando paciente: " + paciente.getNome() + paciente.getPlano() 
+        + paciente.getTelefone() + paciente.getEmail());
         repository.add(paciente);
-        return ResponseEntity.ok("Paciente criado com sucesso!");
+        return ResponseEntity.status(201).body(paciente);
     }
 
-    @GetMapping(path = "/pacientes{id}")
+    // GET
+    @GetMapping(path = "/pacientes/{id}")
     public ResponseEntity<Paciente> get(@PathVariable Long id) {
-        System.out.println("Buscando paciente " + id);
-        var paciente = repository.stream().filter(c -> c.getId().equals(id)).findFirst();
-
-        if (paciente.isEmpty()) {
-            return ResponseEntity.status(404).build();
-        }
-        return ResponseEntity.status(200).body(paciente.get());
+        log.info("Buscando categoria... \n" + id);
+        var paciente = getPaciente(id);
+        return ResponseEntity.ok(paciente);
     }
-    
+
+    // apagar categoria @delete
+    @DeleteMapping("/pacientes/{id}")
+    public ResponseEntity<Object> destroy(@PathVariable Long id) {
+        log.info("Apagando categoria " + id);
+        var paciente = getPaciente(id);
+        repository.remove(paciente);
+        return ResponseEntity.noContent().build();
+
+    }
+
+    // editar categoria @put
+    @PutMapping("/pacientes/{id}")
+    public ResponseEntity<Paciente> update(@PathVariable Long id, @RequestBody Paciente paciente) {
+        log.info("Atualizando categoria " + id + " " + paciente);
+        var pacienteToUpdate = getPaciente(id);
+        repository.remove(pacienteToUpdate);
+        paciente.setId(id);
+        repository.add(paciente);
+        return ResponseEntity.ok(paciente);
+    }
+
+    private Paciente getPaciente(Long id) {
+        return repository.stream()
+                .filter(c -> c.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Paciente não encontrado."));
+    }
+
 }
